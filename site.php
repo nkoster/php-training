6b41a6c146e4a102a12ac0edcac9d84343617631
Niels Koster <n.koster@portavita.eu>
2020-10-03 00:12:15 +0200

    Include php template example

--- site.php
+++ site.php
@@ -7,26 +7,10 @@
 </head>
 <body>
     <?php
-        $index = 1;
-        while ($index <= 5) {
-            echo "$index<br>";
-            $index++;
-        }
-        echo "<hr>";
-        $index = 6;
-        do {
-            echo "$index<br>";
-            $index++;
-        } while ($index <= 5);
-        echo "<hr>";
-        for ($i=1; $i<=5; $i++) {
-            echo "$i<br>";
-        }
-        echo "<hr>";
-        $luckyNumbers = array(4, 8, 14, 16, 23, 42);
-        for ($i=0; $i<count($luckyNumbers); $i++) {
-            echo "$luckyNumbers[$i]<br>";
-        }
+        $title = "First Post";
+        $author = "Niels";
+        $wordcount = 100;
+        include "article-header.php";
     ?>
 </body>
 </html>
