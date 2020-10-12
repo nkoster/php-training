2295eb4e6726a9685f9720a800f42e6357ab1f25
Niels Koster <niels.koster@portavita.nl>
2020-10-12 13:26:22 +0200

    Object with constructor example

--- site.php
+++ site.php
@@ -6,10 +6,21 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        include "tools.php";
-        sayHi("Niels");
-        echo "$feetInMile";
-    ?>
+<?php
+class Book {
+    var $title;
+    var $author;
+    var $pages;
+    function __construct($title, $author, $pages) {
+        $this->title = $title;
+        $this->author = $author;
+        $this->$pages = $pages;
+    }
+}
+$book1 = new Book("Harry Potter", "JK Rowling", 409);
+$book2 = new Book("Rings", "Tokien", 1000);
+echo "$book1->title<br>";
+echo "$book2->title<br>";
+?>
 </body>
 </html>
